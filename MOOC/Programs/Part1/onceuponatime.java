902ba0a1cae06ecd6dea8e6c4556f0f22be9b0cb
noname45688@gmail.com <csaunders5@liberty.edu>
2020-09-30 14:41:57 -0400

    program 3

---
+++ MOOC/Programs/Part1/onceuponatime.java
@@ -0,0 +1,8 @@
+package MOOC.Programs.Part1;
+
+public class onceuponatime {
+
+    public static void main(String[] args) {
+        System.out.println("Once upon a time\nthere was\na program");
+    }
+}
