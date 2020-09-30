3d27c65c68178a74b0263320f900edcfb1e7b714
noname45688@gmail.com <csaunders5@liberty.edu>
2020-09-30 14:44:31 -0400

    MOOC

---
+++ MOOC/Programs/Part1/dinosaur.java
@@ -0,0 +1,7 @@
+package MOOC.Programs.Part1;
+
+public class dinosaur {
+    public static void main(String[] args) {
+        System.out.println("Once upon a time\nthere was\na program");
+    }
+}
