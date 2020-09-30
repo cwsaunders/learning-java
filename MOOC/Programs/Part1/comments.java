cbaa1193a2ea25df9061a4494ea09630b847bf8d
noname45688@gmail.com <csaunders5@liberty.edu>
2020-09-30 14:50:58 -0400

    MOOC

---
+++ MOOC/Programs/Part1/comments.java
@@ -0,0 +1,12 @@
+package MOOC.Programs.Part1;
+
+public class comments {
+    public static void main(String[] args) {
+        System.out.println("Comments");
+        // Single line
+        System.out.println("Comments");
+        /*Multi
+        Line
+        */
+    }
+}
