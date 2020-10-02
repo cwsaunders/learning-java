1a56b9c4c5ddf5c8f624fc69fdbd8a2c1660681c
noname45688@gmail.com <csaunders5@liberty.edu>
2020-10-02 04:06:52 -0400

    mooc

---
+++ MOOC/Programs/Part1/story.java
@@ -0,0 +1,16 @@
+package MOOC.Programs.Part1;
+
+import java.util.Scanner;
+
+public class story {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("I will tell you a story, but I need some information first.\nWhat is the main character called?");
+        String name = scanner.nextLine();
+        System.out.println("What is their job?");
+        String job = scanner.nextLine();
+        System.out.println("Here is the story:");
+        System.out.println("Once upon a time there was " + name + ", who was a " + job + ".");
+        System.out.println("On the way to work, " + name + " reflected on life.\nPerhaps " + name + " will not be a " + job + " forever.");
+    }
+}
