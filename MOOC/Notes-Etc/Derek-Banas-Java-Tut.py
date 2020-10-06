b53c18b6bed8add5681530644566b10f6006e3b5
noname45688@gmail.com <csaunders5@liberty.edu>
2020-10-06 04:24:01 -0400

    mooc

--- MOOC/Notes-Etc/Derek-Banas-Java-Tut.py
+++ MOOC/Notes-Etc/Derek-Banas-Java-Tut.py
@@ -1,4 +1,13 @@
+# Boilerplate
+'''
+String[] <-- if used in terminal the function can recieve a string array
+static <-- function is part of a class
+void <-- Returns nothing (e.g C++)
+args <-- Function that stores String[]
+'''
+
 # Static keyword
 '''
 In public static void main() the 'static' means it is a function belonging to a class
 '''
+
