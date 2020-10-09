ea453604c2d72ae0bbb47acec92accba6c4d722e
noname45688@gmail.com <csaunders5@liberty.edu>
2020-10-09 04:29:11 -0400

    tut

---
+++ DerekBanasTut/Programs/arraylists.java
@@ -0,0 +1,12 @@
+package DerekBanasTut.Programs;
+
+import java.util.*;
+import java.util.stream.IntStream;
+
+public class arraylists {
+    public static void main(String[] args) {
+
+
+
+    }
+}
