c28dd61944ab40b7ad753f5b80dcfc8f110789d6
noname45688@gmail.com <csaunders5@liberty.edu>
2020-10-09 04:24:31 -0400

    tut

--- DerekBanasTut/Programs/javatut.java
+++ DerekBanasTut/Programs/javatut.java
@@ -24,6 +24,22 @@ public class javatut {
         for(int x: oneTo10) System.out.println(x); // Enhanced for loop -- print all values within oneTo10
 
         System.out.println(Arrays.binarySearch(oneTo10, 9)); // Search for '9' in oneTo10
+        
+
+        int a3[][] = new int[2][2]; // Create multi-dimensional array
+        String[][] a4 = {{"00","10"},{"01","11"}}; // multi-dimensional string array
+        
+        System.out.println(a4[1][1]); 
+
+        int a6[] = {1,2,3}; // 1D array assigned 1,2,3
+        int a7[] = Arrays.copyOf(a6,3); // Copy a6
+        System.out.println(Arrays.equals(a6,a7)); // check equals of a6 && a7
+        int a8[] = {3,2,1};
+        Arrays.sort(a8); // sort a8
+        System.out.println(Arrays.toString(a8)); // typecast a8 to string
+
+
+
 
     }
 }
