4d0e81da66cbabca8c18b886558d0d02700f75e7
noname45688@gmail.com <csaunders5@liberty.edu>
2020-10-09 04:32:00 -0400

    tut

--- DerekBanasTut/Programs/arraylists.java
+++ DerekBanasTut/Programs/arraylists.java
@@ -5,7 +5,11 @@ import java.util.stream.IntStream;
 
 public class arraylists {
     public static void main(String[] args) {
-        
+        // Array list allows for resizing and easy creation/deletion
+
+        ArrayList<String> aL1 = new ArrayList<String>(20); // Create aL1 string arraylist
+        aL1.add("sue"); // Add sue to aL1 ArrayList
+        ArrayList<Integer> aL2 = new ArrayList<>(Arrays.asList(1,2,3,4)); //Generate aL2 integer arraylist with 1,2,3, and 4 as values
 
 
     }
